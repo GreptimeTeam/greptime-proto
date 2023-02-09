0f070a451b461076f47c74d1f1275e014408885f
luofucong <luofucong@greptime.com>
2023-02-09 11:25:29 +0800

    move protobuf from GreptimeDB

---
+++ build.rs
@@ -0,0 +1,30 @@
+// Copyright 2023 Greptime Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+fn main() {
+    tonic_build::configure()
+        .compile(
+            &[
+                "proto/greptime/v1/database.proto",
+                "proto/greptime/v1/meta/common.proto",
+                "proto/greptime/v1/meta/heartbeat.proto",
+                "proto/greptime/v1/meta/route.proto",
+                "proto/greptime/v1/meta/store.proto",
+                "proto/greptime/v1/meta/cluster.proto",
+                "proto/prometheus/remote/remote.proto",
+            ],
+            &["proto"],
+        )
+        .expect("compile proto");
+}
