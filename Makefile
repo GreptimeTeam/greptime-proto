36147379c3fd95a10f1cb8a674af1257b7fb5309
zyy17 <zyylsxm@gmail.com>
2023-02-15 15:10:17 +0800

    feat: add go protobuf generation (#6)
    * feat: add go protobuf generation
    
    * ci: check go protbuf generation
    
    * refactor: use 'namely/protoc-all:1.51_1' to compile the proto instead of locally binary
    
    * refactor: no need to add the 'bin/' igore
    
    * chore: modify code format
    
    * refactor: add some comments to describe our modification

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-.PHONY: all rust
+.PHONY: all rust go go-deps
 
-all: rust
+BUILDER_CONTAINER=namely/protoc-all:1.51_1
+
+all: rust go
 
 rust:
 	cargo build
+
+go: go-deps
+	docker run -t -w /greptime-proto \
+    	--entrypoint ./scripts/generate-go.sh \
+    	-v ${PWD}:/greptime-proto ${BUILDER_CONTAINER}
+
+go-deps:
+	go mod download
---
+++ scripts/generate-go.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+set -e
+
+GO_OUTPUT=./go
+
+# TODO(zyy17): Can we make the following commands as one command?
+protoc --go_out=${GO_OUTPUT} --go_opt=paths=source_relative \
+       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
+       -Iproto proto/greptime/v1/meta/*.proto
+
+protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
+       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
+       -Iproto proto/greptime/v1/*.proto
+
+protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
+       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
+       -Iproto proto/prometheus/remote/*.proto
