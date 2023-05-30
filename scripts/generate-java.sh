52dcb43b5ddc6e60492da72ae1f47377b1f78e80
JeremyHi <jiachun_fjc@163.com>
2023-05-30 21:46:06 +0800

    fix: lose prom proto for java (#46)

--- scripts/generate-java.sh
+++ scripts/generate-java.sh
@@ -5,6 +5,7 @@ JAVA_OUTPUT=./java/src/main/java/
 
 protoc -I=${PROTO_ROOT} --java_out=${JAVA_OUTPUT} \
   ${PROTO_ROOT}/greptime/v1/common.proto \
+  ${PROTO_ROOT}/greptime/v1/prom.proto \
   ${PROTO_ROOT}/greptime/v1/column.proto \
   ${PROTO_ROOT}/greptime/v1/ddl.proto \
   ${PROTO_ROOT}/greptime/v1/database.proto
