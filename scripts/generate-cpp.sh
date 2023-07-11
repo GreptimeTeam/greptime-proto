b40c4f26999364cdda3416f64b0c1c83765b21d1
Lei, HUANG <mrsatangel@gmail.com>
2023-07-11 17:36:01 +0800

    fix: compile all protos under ./proto

--- scripts/generate-cpp.sh
+++ scripts/generate-cpp.sh
@@ -5,9 +5,4 @@ set -e
 PROTO_ROOT=./proto
 CPP_OUTPUT=./c++
 
-protoc --proto_path=proto/ --cpp_out=${CPP_OUTPUT}   \
-    ${PROTO_ROOT}/greptime/v1/common.proto   \
-    ${PROTO_ROOT}/greptime/v1/prom.proto   \
-    ${PROTO_ROOT}/greptime/v1/column.proto   \
-    ${PROTO_ROOT}/greptime/v1/ddl.proto   \
-    ${PROTO_ROOT}/greptime/v1/database.proto
+protoc --proto_path=proto/ --cpp_out=${CPP_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
--- scripts/generate-java.sh
+++ scripts/generate-java.sh
@@ -3,9 +3,4 @@
 PROTO_ROOT=./proto
 JAVA_OUTPUT=./java/src/main/java/
 
-protoc -I=${PROTO_ROOT} --java_out=${JAVA_OUTPUT} \
-  ${PROTO_ROOT}/greptime/v1/common.proto \
-  ${PROTO_ROOT}/greptime/v1/prom.proto \
-  ${PROTO_ROOT}/greptime/v1/column.proto \
-  ${PROTO_ROOT}/greptime/v1/ddl.proto \
-  ${PROTO_ROOT}/greptime/v1/database.proto
+protoc -I=${PROTO_ROOT} --java_out=${JAVA_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
