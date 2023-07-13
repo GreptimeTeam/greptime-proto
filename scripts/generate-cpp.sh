f2b48c7fd9a634dcb8b2e5af2e08878a1db7d0bd
Lei, HUANG <6406592+v0y4g3r@users.noreply.github.com>
2023-07-13 19:07:56 +0800

    fix: generate cpp grpc services (#64)

--- scripts/generate-cpp.sh
+++ scripts/generate-cpp.sh
@@ -5,4 +5,5 @@ set -e
 PROTO_ROOT=./proto
 CPP_OUTPUT=./c++
 
-protoc --proto_path=proto/ --cpp_out=${CPP_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
+protoc -I ${PROTO_ROOT} --cpp_out=${CPP_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
+protoc -I ${PROTO_ROOT} --grpc_out=${CPP_OUTPUT} --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $(find  ./proto/  -type f  -iname "*.proto")
