b3db16ff604723edc785cf2f8ef4eadcff75acbb
jiachun <jiachun_fjc@163.com>
2023-08-19 12:13:35 +0800

    feat: from valuedata for value

--- src/v1.rs
+++ src/v1.rs
@@ -14,7 +14,17 @@
 
 tonic::include_proto!("greptime.v1");
 
+use crate::v1::value::ValueData;
+
 pub const GREPTIME_GRPC_DESC: &[u8] = tonic::include_file_descriptor_set!("greptime_grpc_desc");
 
 pub mod meta;
 pub mod region;
+
+impl From<ValueData> for Value {
+    fn from(value: ValueData) -> Self {
+        Value {
+            value_data: Some(value),
+        }
+    }
+}
