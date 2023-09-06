b4f01c6970e4cd94111a1f17d09c053ff1e4af70
niebayes <niebayes@gmail.com>
2023-09-06 17:01:28 +0800

    remove cpp related files

--- scripts/generate-cpp.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-set -e
-
-PROTO_ROOT=./proto
-CPP_OUTPUT=./c++
-
-protoc -I ${PROTO_ROOT} --cpp_out=${CPP_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
-protoc -I ${PROTO_ROOT} --grpc_out=${CPP_OUTPUT} --plugin=protoc-gen-grpc=`which grpc_cpp_plugin` $(find  ./proto/  -type f  -iname "*.proto")
