2240928be9f4e40d18561d3760878a28111d3a41
Ning Sun <sunng@protonmail.com>
2024-07-26 15:31:50 +0800

    feat: add source field to query context

--- c++/greptime/v1/common.pb.h
+++ c++/greptime/v1/common.pb.h
@@ -371,6 +371,7 @@ class QueryContext final :
     kCurrentCatalogFieldNumber = 1,
     kCurrentSchemaFieldNumber = 2,
     kTimezoneFieldNumber = 4,
+    kSourceFieldNumber = 6,
   };
   // map<string, string> extensions = 5;
   int extensions_size() const;
@@ -431,6 +432,15 @@ class QueryContext final :
   std::string* _internal_mutable_timezone();
   public:
 
+  // uint32 source = 6;
+  void clear_source();
+  uint32_t source() const;
+  void set_source(uint32_t value);
+  private:
+  uint32_t _internal_source() const;
+  void _internal_set_source(uint32_t value);
+  public:
+
   // @@protoc_insertion_point(class_scope:greptime.v1.QueryContext)
  private:
   class _Internal;
@@ -447,6 +457,7 @@ class QueryContext final :
     ::PROTOBUF_NAMESPACE_ID::internal::ArenaStringPtr current_catalog_;
     ::PROTOBUF_NAMESPACE_ID::internal::ArenaStringPtr current_schema_;
     ::PROTOBUF_NAMESPACE_ID::internal::ArenaStringPtr timezone_;
+    uint32_t source_;
     mutable ::PROTOBUF_NAMESPACE_ID::internal::CachedSize _cached_size_;
   };
   union { Impl_ _impl_; };
@@ -3420,6 +3431,26 @@ inline ::PROTOBUF_NAMESPACE_ID::Map< std::string, std::string >*
 QueryContext::mutable_extensions() {
   // @@protoc_insertion_point(field_mutable_map:greptime.v1.QueryContext.extensions)
   return _internal_mutable_extensions();
+}
+
+// uint32 source = 6;
+inline void QueryContext::clear_source() {
+  _impl_.source_ = 0u;
+}
+inline uint32_t QueryContext::_internal_source() const {
+  return _impl_.source_;
+}
+inline uint32_t QueryContext::source() const {
+  // @@protoc_insertion_point(field_get:greptime.v1.QueryContext.source)
+  return _internal_source();
+}
+inline void QueryContext::_internal_set_source(uint32_t value) {
+  
+  _impl_.source_ = value;
+}
+inline void QueryContext::set_source(uint32_t value) {
+  _internal_set_source(value);
+  // @@protoc_insertion_point(field_set:greptime.v1.QueryContext.source)
 }
 
 // -------------------------------------------------------------------
