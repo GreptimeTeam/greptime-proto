9d45e1941e07f1eedcc85271ce7d337998939fed
liyang <daviderli614@gmail.com>
2025-07-20 17:33:29 +0000

    chore: generate all go protobuf
    Signed-off-by: liyang <daviderli614@gmail.com>

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -2,21 +2,8 @@
 
 set -e
 
+PROTO_ROOT=./proto
 GO_OUTPUT=./go
 
-# TODO(zyy17): Can we make the following commands as one command?
-protoc --go_out=${GO_OUTPUT} --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/greptime/v1/meta/*.proto
+protoc -I=${PROTO_ROOT} --go_out=paths=source_relative:${GO_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
 
-protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/greptime/v1/*.proto
-
-protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/prometheus/remote/*.proto
-
-protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/substrait_extension/*.proto
