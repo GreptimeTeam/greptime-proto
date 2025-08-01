a6ce3ceb461217dab38af8d9672c4df606ca61fb
liyang <ly18846162402@163.com>
2025-08-01 17:37:20 +0800

    refactor: genetate protobuf (#259)
    * refactor: genetate protobuf
    
    * make all

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -2,17 +2,8 @@
 
 set -e
 
+PROTO_ROOT=./proto
 GO_OUTPUT=./go
 
-# TODO(zyy17): Can we make the following commands as one command?
-protoc --go_out=${GO_OUTPUT} --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/greptime/v1/**/*.proto
+protoc -I=${PROTO_ROOT} --go_out=paths=source_relative:${GO_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
 
-protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/prometheus/**/*.proto
-
-protoc --go_out=${GO_OUTPUT}  --go_opt=paths=source_relative \
-       --go-grpc_out=${GO_OUTPUT}  --go-grpc_opt=paths=source_relative \
-       -Iproto proto/substrait_extension/*.proto
