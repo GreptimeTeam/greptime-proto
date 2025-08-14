2285cc8288ceb945ba4a78dedef1061d65e32c11
liyang <daviderli614@gmail.com>
2025-08-14 17:43:18 +0000

    refactor: re-generate go grpc protobuf
    Signed-off-by: liyang <daviderli614@gmail.com>

--- scripts/generate-go.sh
+++ scripts/generate-go.sh
@@ -6,4 +6,7 @@ PROTO_ROOT=./proto
 GO_OUTPUT=./go
 
 protoc -I=${PROTO_ROOT} --go_out=paths=source_relative:${GO_OUTPUT} $(find  ./proto/  -type f  -iname "*.proto")
+protoc -I=${PROTO_ROOT} \
+       --go-grpc_out=paths=source_relative:${GO_OUTPUT} \
+       $(find ./proto/ -type f -iname "*.proto")
 
